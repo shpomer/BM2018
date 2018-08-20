605f9a0aea5fbe3d017999e52e39abf5a2c1b13a
Filipp Shpomer <filipp.shpomer@wearstrive.com>
2018-08-20 00:21:12 -0700

    fixed build break

--- PalmTreePoofer.ino
+++ PalmTreePoofer.ino
@@ -8,138 +8,114 @@
 // https://github.com/thomasfredericks/Bounce-Arduino-Wiring
 #include <Bounce2.h>
 
-#define BUTTON_TOP_LEFT_PIN 1
-#define BUTTON_TOP_MIDDLE_PIN 3
-#define BUTTON_TOP_RIGHT_PIN 4
-#define BUTTON_BOTTOM_LEFT_PIN 2
-#define BUTTON_BOTTOM_RIGHT_PIN 20
+#define BUTTON_PIN1 1
+#define BUTTON_PIN3 3
+#define BUTTON_PIN4 4
+#define BUTTON_PIN_LEFT 2
+#define BUTTON_PIN_RIGHT 20
 
 #define SOLENOID_LEFT 13
 #define SOLENOID_RIGHT 18
 #define LED_LEFT 19
 #define LED_RIGHT 12
 
-#define MAX_POOF_DURATION 60
-
-elapsedMillis timer;
-
-int leftPoofEndTime = 0;
-int rightPoofEndTime = 0;
-
-bool leftPoofing = 0;
-bool rightPoofing = 0;
-
 // Instantiate a Bounce object
-Bounce debouncer_top_left = Bounce();
-Bounce debouncer_top_middle = Bounce();
-Bounce debouncer_top_right = Bounce();
-Bounce debouncer_bottom_left = Bounce();
-Bounce debouncer_bottom_right = Bounce();
+Bounce debouncer0 = Bounce(); 
+Bounce debouncer1 = Bounce(); 
+Bounce debouncer2 = Bounce(); 
+Bounce debouncer3 = Bounce(); 
+Bounce debouncer4 = Bounce(); 
 
-void left_fire_on(int duration = 0)
-{
-    if(duration < 1 || duration > MAX_POOF_DURATION)
-    {
-        duration = MAX_POOF_DURATION;
-    }
-    leftPoofEndTime = timer + duration;
-    
+void setup() {
+
+  // Setup the button with an internal pull-up :
+  pinMode(BUTTON_PIN1,INPUT_PULLUP);
+  pinMode(BUTTON_PIN3,INPUT_PULLUP);
+  pinMode(BUTTON_PIN4,INPUT_PULLUP);
+  pinMode(BUTTON_PIN_LEFT,INPUT_PULLUP);
+  pinMode(BUTTON_PIN_RIGHT,INPUT_PULLUP);
+
+  pinMode(SOLENOID_RIGHT, OUTPUT);
+  pinMode(SOLENOID_LEFT, OUTPUT);
+
+  // After setting up the button, setup the Bounce instance :
+  debouncer0.attach(BUTTON_PIN1);
+  debouncer1.attach(BUTTON_PIN3);
+  debouncer2.attach(BUTTON_PIN4);
+  debouncer3.attach(BUTTON_PIN_LEFT);
+  debouncer4.attach(BUTTON_PIN_RIGHT);
+  debouncer0.interval(5); // interval in ms
+  debouncer1.interval(5); // interval in ms
+  debouncer2.interval(5); // interval in ms
+  debouncer3.interval(5); // interval in ms
+  debouncer4.interval(5); // interval in ms
+
+  
+}
+
+void loop() {
+  // Update the Bounce instance :
+  debouncer0.update();
+  debouncer1.update();
+  debouncer2.update();
+  debouncer3.update();
+  debouncer4.update();
+  
+  // Get the updated value :
+  int value0 = debouncer0.read();
+  int value1 = debouncer1.read();
+  int value2 = debouncer2.read();
+  int value3 = debouncer3.read();
+  int value4 = debouncer4.read();
+
+  // Turn on or off the LED as determined by the state :
+  if ( value0 == LOW ) {
+    Serial.println("button 0 pressed");
+  } 
+  else {
+  }
+  // Turn on or off the LED as determined by the state :
+  if ( value0 == LOW ) {
+    Serial.println("button 1 pressed");
+    digitalWrite(SOLENOID_LEFT, HIGH);
+  } 
+  else {
+    digitalWrite(SOLENOID_LEFT, LOW);
+  }
+  // Turn on or off the LED as determined by the state :
+  if ( value1 == LOW ) {
+    Serial.println("button 2 pressed");
+    digitalWrite(SOLENOID_RIGHT, HIGH);
+  } 
+  else {
+    digitalWrite(SOLENOID_RIGHT, LOW);
+  }
+  // Turn on or off the LED as determined by the state :
+  if ( value2 == LOW ) {
+    Serial.println("button 3 pressed");
+  } 
+  else {
+  }
+  // Turn on or off the LED as determined by the state :
+  if ( value3 == LOW ) {
+    Serial.println("button LEFT pressed");
     digitalWrite(SOLENOID_LEFT, HIGH);
     digitalWrite(LED_LEFT, HIGH);
-    
-    leftPoofing = true;
-}
-
-void left_fire_off()
-{
+  } 
+  else {
     digitalWrite(SOLENOID_LEFT, LOW);
     digitalWrite(LED_LEFT, LOW);
-    
-    leftPoofing = false;
-}
-
-void right_fire_on(int duration = 0)
-{
-    if(duration < 1 || duration > MAX_POOF_DURATION)
-    {
-        duration = MAX_POOF_DURATION;
-    }
-    rightPoofEndTime = timer + duration;
-    
+  }
+  // Turn on or off the LED as determined by the state :
+  if ( value4 == LOW ) {
+    Serial.println("button RIGHT pressed");
     digitalWrite(SOLENOID_RIGHT, HIGH);
     digitalWrite(LED_RIGHT, HIGH);
-    
-    rightPoofing = true;
-}
-
-void right_fire_off()
-{
+  } 
+  else {
     digitalWrite(SOLENOID_RIGHT, LOW);
     digitalWrite(LED_RIGHT, LOW);
-    
-    rightPoofing = false;
-}
-
-void setup()
-{
-    // Setup the button with an internal pull-up :
-    pinMode(BUTTON_TOP_LEFT_PIN,INPUT_PULLUP);
-    pinMode(BUTTON_TOP_MIDDLE_PIN,INPUT_PULLUP);
-    pinMode(BUTTON_TOP_RIGHT_PIN,INPUT_PULLUP);
-    pinMode(BUTTON_BOTTOM_LEFT_PIN,INPUT_PULLUP);
-    pinMode(BUTTON_BOTTOM_RIGHT_PIN,INPUT_PULLUP);
-
-    // After setting up the button, setup the Bounce instance :
-    debouncer_top_left.attach(BUTTON_TOP_LEFT_PIN);
-    debouncer_top_middle.attach(BUTTON_TOP_MIDDLE_PIN);
-    debouncer_top_right.attach(BUTTON_TOP_RIGHT_PIN);
-    debouncer_bottom_left.attach(BUTTON_BOTTOM_LEFT_PIN);
-    debouncer_bottom_right.attach(BUTTON_BOTTOM_RIGHT_PIN);
-  
-    debouncer_top_left.interval(5); // interval in ms
-    debouncer_top_middle.interval(5); // interval in ms
-    debouncer_top_right.interval(5); // interval in ms
-    debouncer_bottom_left.interval(5); // interval in ms
-    debouncer_bottom_right.interval(5); // interval in ms
-
-    left_fire_off();
-    right_fire_off();
-}
-
-void loop()
-{
-    // Update the Bounce instance :
-    debouncer_top_left.update();
-    debouncer_top_middle.update();
-    debouncer_top_right.update();
-    debouncer_bottom_left.update();
-    debouncer_bottom_right.update();
-
-    // Get the updated value :
-    bool top_left_pressed = debouncer_top_left.fallingEdge();
-    bool top_middle_pressed = debouncer_top_middle.fallingEdge();
-    bool top_right_pressed = debouncer_top_right.fallingEdge();
-    bool bottom_left_pressed = debouncer_bottom_left.fallingEdge();
-    bool bottom_right_pressed = debouncer_bottom_right.fallingEdge();
-
-    if ( leftPoofing && timer > leftPoofStart + POOF_DURATION )
-    {
-        left_fire_off();
-    }
-
-    // Turn on or off the LED as determined by the state :
-    if ( top_left_pressed && !leftPoofing && !rightPoofing ) {
-        Serial.println("button 0 pressed");
-        if(!leftPoofing)
-        {
-            digitalWrite(SOLENOID_LEFT, HIGH);
-            digitalWrite(SOLENOID_RIGHT, HIGH);
-            digitalWrite(LED_RIGHT, HIGH);
-            digitalWrite(LED_LEFT, HIGH);
-            leftPoofStart = timer;
-            leftPoofing = 1;
-        }
-  } 
+  }
   
 }
 
